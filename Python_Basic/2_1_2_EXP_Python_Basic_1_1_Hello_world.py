5101a4fc09ff71bc02c90ae5a3d284166442a08d
jeffrocha <jeff.r.silva@hotmail.com>
2022-11-22 11:45:39 -0300

    .

---
+++ Python_Basic/2_1_2_EXP_Python_Basic_1_1_Hello_world.py
@@ -0,0 +1,7 @@
+print('Hello world')  # o conteudo está atribuido pelo código ascII
+print("Hello World")  # o conteudo está atribuido por texto
+print("""hello
+
+
+
+ wolrd""")  # o conteudo está atribuido por texto com opções de grande quebra de linha
---
+++ Python_Basic/2_1_2_EXP_Python_Basic_1_2_comments.py
@@ -0,0 +1,7 @@
+# o caracterio cerquilha serve para comentario curto de apenas UMA LINHA
+print("hello wolrd")
+"""
+o triplo aspas serve
+para comentarios ou print
+de multiplas linhas
+"""
